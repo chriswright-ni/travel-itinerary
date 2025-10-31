d72ad91c5414944e2504b2a5f50d710cc9b38c68
Chris Wright <chris@Chriss-MacBook-Pro.local>
2025-10-31 13:05:25 +0000

    wip: Landing page results grid

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,6 +1,8 @@
 import SearchBar from "../components/SearchBar";
+import PlaceCard from "../components/PlaceCard";
 import "../css/LandingPage.css";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 function LandingPage() {
   return (
@@ -14,7 +16,23 @@ function LandingPage() {
         }}
       >
         <SearchBar />
+     
+        <Grid container spacing={2}>
+          <Grid size={6}>
+            <PlaceCard />
+          </Grid>
+          <Grid size={6}>
+            <PlaceCard />
+          </Grid>
+          <Grid size={6}>
+            <PlaceCard />
+          </Grid>
+          <Grid size={6}>
+            <PlaceCard />
+          </Grid>
+        </Grid>
       </Box>
+     
     </>
   );
 }
