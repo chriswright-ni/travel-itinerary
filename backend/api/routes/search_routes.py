4d4c76de3f543840f69d1e89ece5c241bf5d7595
Chris Wright <chris@Chriss-MacBook-Pro.local>
2025-11-16 22:14:28 +0000

    wip: Backend route created for Mapbox Search API calls

--- backend/api/routes/search_routes.py
+++ backend/api/routes/search_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from dotenv import load_dotenv
 import requests
 import os
@@ -7,6 +7,29 @@ search_bp = Blueprint("search_routes", __name__)
 
 load_dotenv()
 PLACES_API_KEY = os.getenv("PLACES_API_KEY")
+MAPBOX_ACCESS_TOKEN = os.getenv("MAPBOX_ACCESS_TOKEN")
+
+# This route uses Mapbox search API to return a list of locations based on the users search
+@search_bp.route("/api/location/suggest", methods=["GET"])
+def suggest():
+  print("inside suggest route")
+  search_text = request.args.get("q")
+  session_token = "test_session"
+
+  url = "https://api.mapbox.com/search/searchbox/v1/suggest"
+  params = {
+    "q": search_text,
+    "session_token": session_token,
+    "access_token": MAPBOX_ACCESS_TOKEN,
+    "types": "city,region,place,street,address,postcode,district",
+    "limit": 10
+  }
+
+  response = requests.get(url, params=params)
+  print(response)
+  data = response.json()
+
+  return data
 
 @search_bp.route("/hello", methods=["GET"])
 def hello():
@@ -28,7 +51,7 @@ def search():
     place_data = data.get("results", []) # The second argument, [], is the default value if the get method produces a type error
 
 
-    # The results data from thr Foursquare Places API is an array of objects
+    # The results data from the Foursquare Places API is an array of objects
     # Cleaning the data to remove unnecessary fields - creating a smaller array of objects
     places = []
     for i, place in enumerate(place_data):
@@ -42,41 +65,39 @@ def search():
     return jsonify(places)
 
 
-# @search_bp.route("/search", methods=["GET"])
-# def search():
-#     data = {
-#       "places": [
-#         {
-#           "id": 1,
-#           "name": "Central Park",
-#           "location": "New York City, USA",
-#           "description": "A large public park in Manhattan known for its scenic landscapes, walking paths, and cultural landmarks."
-#         },
-#         {
-#           "id": 2,
-#           "name": "Eiffel Tower",
-#           "location": "Paris, France",
-#           "description": "An iconic iron tower built in 1889, offering panoramic views of Paris and a symbol of French culture."
-#         },
-#         {
-#           "id": 3,
-#           "name": "Mount Fuji",
-#           "location": "Honshu, Japan",
-#           "description": "Japan’s tallest mountain and a sacred symbol, popular for hiking and photography."
-#         },
-#         {
-#           "id": 4,
-#           "name": "Santorini",
-#           "location": "Cyclades Islands, Greece",
-#           "description": "A volcanic island known for its whitewashed buildings, blue-domed churches, and beautiful sunsets."
-#         },
-#         {
-#           "id": 5,
-#           "name": "Machu Picchu",
-#           "location": "Cusco Region, Peru",
-#           "description": "An ancient Incan city set high in the Andes Mountains, renowned for its archaeological significance and breathtaking views."
-#         }
-#       ]
-# }
+@search_bp.route("/places", methods=["GET"])
+def get_places():
 
-#     return jsonify(data)
+    # Get interest category from query string
+    interest_category = request.args.get("interestCategory")
+
+    if not interest_category:
+      print("No interest category selected") # Update this
+
+    url = "https://places-api.foursquare.com/places/search"
+
+    headers = {
+        "accept": "application/json",
+        "X-Places-Api-Version": "2025-06-17",
+        "Authorization": f"Bearer {PLACES_API_KEY}"
+    }
+    params = {f"query": {interest_category}, "ll": "54.4203,-6.4548", "radius": 5000, "limit": 50} # FSQ places result limit is 50, unless pagination is used
+    
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+    place_data = data.get("results", []) # The second argument, [], is the default value if the get method produces a type error
+    print(f"Finding places with interest category: {interest_category}")
+
+    # The results data from the Foursquare Places API is an array of objects
+    # Cleaning the data to remove unnecessary fields - creating a smaller array of objects
+    places = []
+    for i, place in enumerate(place_data):
+      place_cleaned = {
+        "id": i + 1,
+        "name": place.get("name")
+      }
+      print(place.get("name"))
+      places.append(place_cleaned);
+
+    return jsonify(places)
+
